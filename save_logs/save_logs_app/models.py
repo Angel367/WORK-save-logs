255eece3bd876e525b37af65378abb9ee0894dee
Volkov Egor Alexandrovich <egor.volkov20038@gmail.com>
2024-07-20 01:22:17 +0300

    first test x10

--- save_logs/save_logs_app/models.py
+++ save_logs/save_logs_app/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class SearchLog(models.Model):
-    search_query = models.CharField(max_length=255)
+    search_query = models.CharField(max_length=255, null=True, blank=True)
     search_date = models.DateTimeField(auto_now_add=True)
-    search_ip = models.GenericIPAddressField()
-    path = models.CharField(max_length=255)
-    status_code = models.IntegerField()
+    search_ip = models.GenericIPAddressField(null=True, blank=True)
+    path = models.CharField(max_length=255, null=True, blank=True)
+    status_code = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.search_query
